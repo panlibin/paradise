4843c1eaa82c6d6f4b9b1aeb2b0b7d35a3660a51
neon <panlibin@onemt.com.cn>
2017-03-21 15:07:41 +0800

    no message

--- launchconfig.lua
+++ launchconfig.lua
@@ -1,13 +1,5 @@
-gameplay_data_path = "./gameplay/data/?.lua;"
+include("pathconfig.lua")
 
-gameplay_lua_path = "./gameplay/envconfig/?.lua;"
-	.."./gameplay/lualib/?.lua;"
-	.."./gameplay/service_logic/?.lua;"
-	.."./gameplay/service_proxy/?.lua;"
-
-gameplay_service_path = "./gameplay/service/?.lua;"
-
-root = "./"
 thread = 8
 logservice = "snlua"
 logger = "logger"
@@ -15,10 +7,5 @@ logpath = "./log/"
 harbor = 0
 start = "main"
 bootstrap = "snlua bootstrap"
-luaservice = root.."skynet/service/?.lua;"..gameplay_service_path
-lua_path = root.."skynet/lualib/?.lua;"..gameplay_data_path..gameplay_lua_path
-lua_cpath = root.."skynet/luaclib/?.so;".."./gameplay/luaclib/?.so;"
-cpath = root.."skynet/cservice/?.so;"
-lualoader = root.."skynet/lualib/loader.lua"
 preload = "./gameplay/preload.lua"	-- run preload.lua before every lua service run
 -- daemon = "./skynet.pid"
---
+++ pathconfig.lua
@@ -0,0 +1,16 @@
+luaservice = "./skynet/service/?.lua;"
+	.."./gameplay/service/?.lua;"
+
+lua_path = "./skynet/lualib/?.lua;"
+	.."./gameplay/lualib/?.lua;"
+	.."./gameplay/service_logic/?.lua;"
+	.."./gameplay/service_proxy/?.lua;"
+	.."./gameplay/data/?.lua;"
+	.."./gameplay/envconfig/?.lua;"
+
+lua_cpath = "./skynet/luaclib/?.so;"
+	.."./gameplay/luaclib/?.so;"
+
+cpath = "./skynet/cservice/?.so;"
+
+lualoader = "./skynet/lualib/loader.lua"
